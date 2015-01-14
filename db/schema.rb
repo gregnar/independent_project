c6744d27a45b01b1de90d149c987b300705038f8
Greg Narajka <gregnar@gmail.com>
2015-01-14 12:54:21 -0700

    got dashboard books functional, needs to be prettier

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114160120) do
+ActiveRecord::Schema.define(version: 20150114183949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.integer  "isbn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "goodreads_id"
