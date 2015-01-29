17bb64ba0a7a5c2e53004051465d3c325ad59c0f
Greg Narajka <gregnar@gmail.com>
2015-01-28 21:20:42 -0700

    could use a few tweaks but essentially ready for eval

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,9 @@ class DashboardController < ApplicationController
   before_action :require_current_user
 
   def index
-    @books               = current_user.books.take(6)
-    @followees           = current_user.followees.take(8)
-    @suggested_followees = current_user.suggested_followees.sort_by(&:goodreads_id).take(6)
+    @books               = current_user.books.take(3)
+    @followees           = current_user.followees.take(4)
+    @suggested_followees = current_user.suggested_followees.sort_by(&:goodreads_id).take(4)
   end
 
 end
